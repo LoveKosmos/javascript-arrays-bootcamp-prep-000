bf3c1236ecb186daae124fb81ddcbf5053cebc75
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 17:23:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array
+}
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+function addElementToEndOfArray(array, element){
+  return [ ...array, element];
+}
